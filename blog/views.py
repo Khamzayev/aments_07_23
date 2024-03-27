2a02515a3e712935b9efc94baf2f5acbacf0f6e0
Khamzayev <xamzayevnodirbek@gmail.com>
2024-03-27 16:37:24 +0500

    blog add views.py blog, add templates name change

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.views.generic import ListView, CreateView, DetailView
-from blog.models import  Author, Post, Tags, Comment
+from blog.models import  Author, Post
 from django.urls import reverse
 from blog.forms import CommentForm
 from typing import Any
 from django.db.models.query import QuerySet
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
 
+
+
+class Blog(ListView):
+    template_name = 'blog.html'
