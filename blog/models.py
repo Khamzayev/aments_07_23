f9f7546ca2096c891a678cc0ab4f424ea82cdf63
Khamzayev <xamzayevnodirbek@gmail.com>
2024-03-27 14:23:10 +0500

    blog add abstract.py models, author modelsadd

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from blog.abstract import ActiveModel, OrderModel, BaseModel
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+
+class Author(BaseModel):
+    full_name = models.CharField(max_length=100, verbose_name=_("full_name"))
+    image = models.ImageField(upload_to='media/blog/author/%Y/%m/%d')
+
+    def __str__(self) -> str:
+        return self.full_name
+    
+    class Meta:
+        verbose_name = _("Author")
+        verbose_name_plural = _("Authors")
+
+
+
+
