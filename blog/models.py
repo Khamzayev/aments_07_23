518307a20cfb6f52ac423006a0b3aac1127b37c5
Khamzayev <xamzayevnodirbek@gmail.com>
2024-03-27 15:21:32 +0500

    blog add models BlogFullWidth

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,16 @@ class Author(BaseModel):
 
 
 
+class BlogFullWidth(BaseModel):
+    title = models.CharField(max_length=100, help_text=_("write title"), verbose_name=_("title"))
+    author = models.CharField(max_length=30, help_text=_("write author name"), verbose_name=_("author"))
+    image = models.ImageField(upload_to='media/blog/post/%Y/%m/%d', verbose_name=_("image"))
+
+    def __str__(self) -> str:
+        return f"{self.title}, {self.author}"
+    
+    class Meta:
+        verbose_name = _("Blog")
+        verbose_name_plural = _("Blogs")
+
+        
