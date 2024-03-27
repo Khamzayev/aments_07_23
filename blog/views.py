259bcf88bb528626db1a78416daca9a3098b809f
Khamzayev <xamzayevnodirbek@gmail.com>
2024-03-27 16:30:34 +0500

    blog add forms.py commentform, add views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView
+from blog.models import  Author, Post, Tags, Comment
+from django.urls import reverse
+from blog.forms import CommentForm
+from typing import Any
+from django.db.models.query import QuerySet
 
-# Create your views here.
