c3f1c556867b2147aff052937b7bba3cad90fcaf
Khamzayev <xamzayevnodirbek@gmail.com>
2024-04-02 06:33:30 +0500

    add urls.py, create detail.views in blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from blog import views
+from blog.views import  Index,Blog,BlogDetail, CommentCreate
 
 
-#app_name = 'blog'
+app_name = 'blog'
 
 urlpatterns = [    
-    
+    path("index/", Index.as_view()),
+    path("", Blog.as_view(), name='blog'), #blog/1/blog_detail
+    path("<int:pk>/blog_detail/", BlogDetail.as_view(), name='blog_detail'),
+    path("<int:pk>/comment/", CommentCreate.as_view(), name='comment')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, TemplateView
 from blog.models import  Author, Post
 from django.urls import reverse
 from blog.forms import CommentForm
@@ -12,3 +14,25 @@ from django.shortcuts import get_object_or_404
 
 class Blog(ListView):
     template_name = 'blog.html'
+
+class Index(TemplateView):
+    template_name = 'layouts/base.html'  
+
+class BlogDetail(DetailView):
+    model = Post
+    template_name = 'blog/blog-detail.html'  
+
+    def get_queryset(self) -> QuerySet[Any]:
+        qs = Post.objects.order_by("-id")
+        tag = self.request.GET.get("tag")
+        if tag:
+            qs = qs.filter(tag__name=tag)
+        return qs
+    
+class CommentCreate(CreateView):
+    form_class = CommentForm
+    def form_valid(self, form):
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs.get('pk'))
+
+        return  
+      
